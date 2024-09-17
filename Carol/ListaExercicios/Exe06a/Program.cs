04f2032096cdba7807c2b6cb47bbc93c7db51d4b
ANA CAROLINE FERREIRA <112591071+Carolinefp@users.noreply.github.com>
2024-09-16 21:15:26 -0300

    Add files via upload

---
+++ Carol/ListaExercicios/Exe06a/Program.cs
@@ -0,0 +1,43 @@
+﻿int tamanho = 100;
+int[] vetor = new int[tamanho];
+
+//vetor valores aleatorios
+Random random = new Random();
+for (int i = 0; i < tamanho; i++)
+{
+
+    vetor[i] = random.Next(100);
+
+}
+// imprimir o vetor sem ordenação
+for (int i = 0; i < tamanho; i++)
+{
+
+    Console.Write(vetor[i] + " ");
+
+}
+
+bool troca = false;
+do
+{
+    for (int i = 0; i < tamanho - 1; i++)
+    {
+
+        if (vetor[i] > vetor[i + 1])
+        {
+            int aux = vetor[i];
+            vetor[i] = vetor[i + 1];
+            vetor[i + 1] = aux;
+            troca = true;
+        }
+
+    }
+} while (troca);
+
+// imprimir o vetor ordenadas
+for (int i = 0; i < tamanho; i++)
+{
+
+    Console.Write(vetor[i] + " ");
+
+}
---
+++ Carol/README.md
@@ -0,0 +1,2 @@
+# Projeto-Software-Visual
+Aulas de segunda
